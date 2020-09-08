e3d6cce0cbd724b07549a55f519a7f14617a96c2
RipleyTom <RipleyTom@users.noreply.github.com>
2020-09-08 09:07:31 +0200

    Nuke harden warning
    Note that we actually are building the library hardened.
    Ideally would need a better way to generate defines header in CMake.

--- wolfssl/wolfcrypt/settings.h
+++ wolfssl/wolfcrypt/settings.h
@@ -2043,19 +2043,19 @@ extern void uITRON4_free(void *p) ;
 #endif
 
 /* warning for not using harden build options (default with ./configure) */
-#ifndef WC_NO_HARDEN
-    #if (defined(USE_FAST_MATH) && !defined(TFM_TIMING_RESISTANT)) || \
-        (defined(HAVE_ECC) && !defined(ECC_TIMING_RESISTANT)) || \
-        (!defined(NO_RSA) && !defined(WC_RSA_BLINDING) && !defined(HAVE_FIPS) && \
-            !defined(WC_NO_RNG))
+// #ifndef WC_NO_HARDEN
+//     #if (defined(USE_FAST_MATH) && !defined(TFM_TIMING_RESISTANT)) || \
+//         (defined(HAVE_ECC) && !defined(ECC_TIMING_RESISTANT)) || \
+//         (!defined(NO_RSA) && !defined(WC_RSA_BLINDING) && !defined(HAVE_FIPS) && \
+//             !defined(WC_NO_RNG))
 
-        #ifndef _MSC_VER
-            #warning "For timing resistance / side-channel attack prevention consider using harden options"
-        #else
-            #pragma message("Warning: For timing resistance / side-channel attack prevention consider using harden options")
-        #endif
-    #endif
-#endif
+//         #ifndef _MSC_VER
+//             #warning "For timing resistance / side-channel attack prevention consider using harden options"
+//         #else
+//             #pragma message("Warning: For timing resistance / side-channel attack prevention consider using harden options")
+//         #endif
+//     #endif
+// #endif
 
 #if defined(NO_OLD_WC_NAMES) || defined(OPENSSL_EXTRA)
     /* added to have compatibility with SHA256() */
